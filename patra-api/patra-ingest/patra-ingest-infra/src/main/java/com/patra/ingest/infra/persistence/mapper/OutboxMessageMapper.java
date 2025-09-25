64464394c97f5e15d7320e6fc66748c5f19622c6
Lin <1214219989@qq.com>
2025-09-25 17:04:28 +0800

    Add IngOutboxMessageDO and OutboxMessageMapper: implement outbox message data object and mapper for persistence

---
+++ patra-api/patra-ingest/patra-ingest-infra/src/main/java/com/patra/ingest/infra/persistence/mapper/OutboxMessageMapper.java
@@ -0,0 +1,7 @@
+package com.patra.ingest.infra.persistence.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.patra.ingest.infra.persistence.entity.IngOutboxMessageDO;
+
+public interface OutboxMessageMapper extends BaseMapper<IngOutboxMessageDO> {
+}
