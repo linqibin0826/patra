d3aaf240728596559db338a536b37caa8c938c6f
Lin <1214219989@qq.com>
2025-09-26 10:20:43 +0800

    Add database initialization scripts and update application configuration: include SQL scripts for creating databases and user privileges; enhance application.yaml with datasource and Flyway configurations.

---
+++ patra-api/docker/mysql/init/Papertrace_init.sql
@@ -0,0 +1,3 @@
+create database patra_ingest default character set utf8mb4 collate utf8mb4_0900_ai_ci;
+create database patra_registry default character set utf8mb4 collate utf8mb4_0900_ai_ci;
+create database patra_category default character set utf8mb4 collate utf8mb4_0900_ai_ci;
