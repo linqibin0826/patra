2db606222b413b45abf0ace08c429209d5df7967
Lin <1214219989@qq.com>
2025-10-18 01:31:08 +0800

    feat(expr): update error handling auto-configuration; refactor ExprCompiler and adjust plan documentation

--- patra-spring-boot-starter-expr/src/main/java/com/patra/starter/expr/compiler/ExprCompiler.java
+++ patra-spring-boot-starter-expr/src/main/java/com/patra/starter/expr/compiler/ExprCompiler.java
@@ -5,7 +5,6 @@ import com.patra.expr.*;
 import com.patra.starter.expr.compiler.model.CompileRequest;
 import com.patra.starter.expr.compiler.model.CompileRequestBuilder;
 import com.patra.starter.expr.compiler.model.CompileResult;
-import java.util.List;
 
 public interface ExprCompiler {
 
@@ -62,25 +61,5 @@ public interface ExprCompiler {
             .forOperation(endpointName)
             .build();
     return compile(request);
-  }
-
-  default boolean containsAtom(Expr expression) {
-    return containsAtomInternal(expression);
-  }
-
-  private static boolean containsAtomInternal(Expr expr) {
-    if (expr instanceof Atom) {
-      return true;
-    }
-    if (expr instanceof And(List<Expr> children)) {
-      return children.stream().anyMatch(ExprCompiler::containsAtomInternal);
-    }
-    if (expr instanceof Or(List<Expr> children)) {
-      return children.stream().anyMatch(ExprCompiler::containsAtomInternal);
-    }
-    if (expr instanceof Not(Expr child)) {
-      return containsAtomInternal(child);
-    }
-    return false;
   }
 }
